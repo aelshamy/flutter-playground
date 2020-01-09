41a274d58f1730e55fa22602aca2f59187402602
Amr Elshamy <elshaa1@nationwide.com>
2020-01-08 23:50:03 -0500

    add comments section

--- socialmedia/lib/main.dart
+++ socialmedia/lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/home.dart';
 
 void main() {
-  Firestore.instance.settings();
   runApp(MyApp());
 }
 

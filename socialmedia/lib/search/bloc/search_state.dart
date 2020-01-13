3627bdc301b7acae12cca60afe2e7b38ce00e0e0
Amr Elshamy <ammohamed@icct.com>
2020-01-12 21:39:53 -0500

    update search state

--- socialmedia/lib/search/bloc/search_state.dart
+++ socialmedia/lib/search/bloc/search_state.dart
@@ -1,7 +1,9 @@
 import 'package:equatable/equatable.dart';
 
 abstract class SearchState extends Equatable {
-  const SearchState();
+  const SearchState([List props = const []]);
+  @override
+  List<Object> get props => this.props;
 }
 
 class InitialSearchState extends SearchState {

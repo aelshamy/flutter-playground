8e0de93e051362d13ffe03676a6dff01f6836403
Amr Elshamy <elshaa1@nationwide.com>
2020-01-22 01:17:53 -0500

    update log

--- socialmedia/lib/common/simple_bloc_delegate.dart
+++ socialmedia/lib/common/simple_bloc_delegate.dart
@@ -6,18 +6,18 @@ class SimpleBlocDelegate extends BlocDelegate {
   @override
   void onEvent(Bloc bloc, Object event) {
     super.onEvent(bloc, event);
-    log(event.toString());
+    log("event: $event");
   }
 
   @override
   void onError(Bloc bloc, Object error, StackTrace stacktrace) {
     super.onError(bloc, error, stacktrace);
-    log(error.toString());
+    log("error: $error");
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    log(transition.toString());
+    log("$transition");
   }
 }

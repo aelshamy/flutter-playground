fd1220cb07c123718e2a0357483e7f8f24e240de
Amr Elshamy <elshaa1@nationwide.com>
2020-01-28 15:49:07 -0500

    update prefrences

--- daynamic_theme/lib/theme_preferences.dart
+++ daynamic_theme/lib/theme_preferences.dart
@@ -7,16 +7,14 @@ class ThemePreferences {
   static const DARK_MODE_ENABLED = "darkModeEnabled";
 
   Future<SharedPreferences> _getInstance() async {
-    return _prefs ?? await SharedPreferences.getInstance();
+    return _prefs ??= await SharedPreferences.getInstance();
   }
 
   Future<void> setDarkMode(bool value) async {
-    final _instance = await _getInstance();
-    await _instance.setBool(DARK_MODE_ENABLED, value);
+    (await _getInstance()).setBool(DARK_MODE_ENABLED, value);
   }
 
   Future<bool> isDarkModeEnabled() async {
-    final _instance = await _getInstance();
-    return _instance.getBool(DARK_MODE_ENABLED) ?? false;
+    return (await _getInstance()).getBool(DARK_MODE_ENABLED) ?? false;
   }
 }

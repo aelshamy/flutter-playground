394bd2afbace6b6559f45bf39a2109628e481b59
Amr Elshamy <amr-hussien@hotmail.com>
2023-10-12 01:45:41 -0400

    add Telegram undo

--- flutter_undo_action/lib/main.dart
+++ flutter_undo_action/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Telegram undo',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const Home(),
     );
